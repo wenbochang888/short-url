398c850b3cfc6243cd44deb82626ca8de9e283b9
wenbo.chang <wenbo.chang@huolala.cn>
2025-02-10 17:09:32 +0800

    echo

--- src/main/java/com/wenbo/shorturl/controller/ShortUrlController.java
+++ src/main/java/com/wenbo/shorturl/controller/ShortUrlController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -38,5 +39,11 @@ public class ShortUrlController {
 		model.addAttribute("shortUrl", shortUrl);
 
 		return "result";
+	}
+
+	@ResponseBody
+	@RequestMapping("/")
+	public String mp(String echostr) {
+		return echostr;
 	}
 }
